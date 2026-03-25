909cbd3afb826bfb0b94aa64bba4209e4cf5a8f3
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-03-24 22:29:46 -0400

    update h file

--- include/kernel/memory/page_tables.h
+++ include/kernel/memory/page_tables.h
@@ -6,15 +6,15 @@
 typedef uint64_t pte_t;
 
 typedef struct page_table {
-    pte_t page_table_entries[NUM_PTE];
+  pte_t page_table_entries[NUM_PTE];
 } page_table_t;
 
-extern page_table_t* root_page_table;
+extern page_table_t *root_page_table;
 
 void allocate_root_page_table();
 
-page_table_t* allocate_page_table();
-void create_page_table_entry(uint64_t pa);
+page_table_t *allocate_page_table();
+void create_page_table_entry(uint64_t va, uint64_t pa);
 void remove_page_table_entry(uint64_t pa);
 
 void create_identity_map();
