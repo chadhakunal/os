48d0302903473268cf0802523f00f8c32ae4fea3
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-03-24 21:48:57 -0400

    incude types

--- include/kernel/drivers/uart.h
+++ include/kernel/drivers/uart.h
@@ -1,8 +1,10 @@
 #ifndef KERNEL_DRIVERS_UART
 #define KERNEL_DRIVERS_UART
 
+#include "types.h"
+
 void uart_putc(const char c);
-void uart_print(const char* c);
+void uart_print(const char *c);
 void uart_print_hex(const uint64_t value);
 void uart_print_hex_32(const uint32_t value);
 void uart_print_int(int32_t value);
