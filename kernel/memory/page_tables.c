dcf1dcf84b220807cafcfe05eb5ab448b9d9a56d
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-03-29 00:22:54 -0400

    switch to llu

--- kernel/memory/page_tables.c
+++ kernel/memory/page_tables.c
@@ -102,7 +102,7 @@ void init_page_mapping() {
   /* Map UART device for MMIO access after MMU is enabled */
   uint64_t uart_phys = (uint64_t)uart_get_base(); /* align to page */
   uint64_t uart_virt = MMIO_VIRTUAL_MEMORY_BASE;
-  printk("uart_virt: %lx, uart_phys: %lx\n", uart_virt, uart_phys);
+  printk("uart_virt: %llu, uart_phys: %llu\n", uart_virt, uart_phys);
   if (platform.uart.base != 0) {
     /* Map one page containing the UART device */
     create_page_table_entry(uart_virt, uart_phys);
