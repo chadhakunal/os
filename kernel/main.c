e6327f8ffe86e5b1f357d394097c8f3048428545
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-04-04 15:59:44 -0400

    logs

--- kernel/main.c
+++ kernel/main.c
@@ -7,6 +7,7 @@
 #include "kernel/memory/memory_info.h"
 #include "kernel/memory/page_allocator.h"
 #include "kernel/memory/page_tables.h"
+#include "virtual_memory_init.h"
 
 #include "lib/printk/printk.h"
 
@@ -33,6 +34,13 @@ void kmain(void *dtb_ptr) {
   init_page_mapping();
 
   printk("Virtual Memory Enabled and we are still running!\n");
+
+  uint64_t current_sp;
+  asm volatile("mv %0, sp" : "=r"(current_sp));
+  printk("Current SP in kmain: %llx\n", current_sp);
+  printk("Kernel mapped range: %llx to %llx\n",
+         KERNEL_VIRTUAL_MEMORY_BASE,
+         KERNEL_VIRTUAL_MEMORY_BASE + (memory_info.kernel_end - memory_info.kernel_start));
 
   remove_identity_mapping();
 
