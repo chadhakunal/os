480fc1ca1b774ca772e21f01de2e21322a28675e
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-04-05 22:30:23 -0400

    fix

--- lib/pool_allocator.c
+++ lib/pool_allocator.c
@@ -4,15 +4,15 @@
 void *pool_alloc(struct pool *allocating_pool) {
   if (allocating_pool->free_list == NULL) {
     void *free_page = get_page(true);
-    uint64_t num_objs = PAGE_SIZE / allocating_pool->obj_size;
+    uint64_t num_objs = DEFAULT_PAGE_SIZE  / allocating_pool->obj_size;
     for (uint64_t i = 0; i < num_objs; i++) {
-      struct pool_node *pn = (struct pool_node *) free_page + (i + obj_size);
+      struct pool_node *pn = (struct pool_node *) free_page + (i + allocating_pool->obj_size);
       if (allocating_pool->free_list == NULL) {
-        allocating_pool->free_list = pool_node;
+        allocating_pool->free_list = pn;
         pool_node->next = NULL;
       } else {
         pool_node->next = allocating_pool->free_list;
-        allocating_pool->pool_node;
+        allocating_pool->pool_node = pn;
       }
     }
   }
