d897617a045cc44305eb4aee04e2fbca945bf6fb
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-04-15 17:15:44 -0400

    print vnode

--- kernel/filesystem/tarfs/tarfs.c
+++ kernel/filesystem/tarfs/tarfs.c
@@ -7,6 +7,6 @@
 struct superblock_t *tarfs_mount(void *data, uint64_t size) {
   struct superblock_t *tarfs_superblock = superblock_t_alloc();
   parse_tar(data, size, tarfs_superblock);
-  print_vnode(tarfs_superblock->root_vnode);
+  vfs_print_vnode(tarfs_superblock->root_vnode);
   return tarfs_superblock;
 }
