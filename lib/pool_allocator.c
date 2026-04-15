186913f7cf3eed0bf13d6ae4b0eed0fe4076de2a
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-04-15 17:43:11 -0400

    add prints to pool alloc

--- lib/pool_allocator.c
+++ lib/pool_allocator.c
@@ -1,9 +1,11 @@
 #include "lib/pool_allocator.h"
 #include "kernel/memory/page_allocator.h"
+#include "lib/printk/printk.h"
 
 void *pool_alloc(struct pool *allocating_pool) {
   if (allocating_pool->free_list == NULL) {
     void *free_page = get_page(true);
+    printk("pool_alloc: allocated new page at %p for obj_size=%lld\n", free_page, allocating_pool->obj_size);
     uint64_t num_objs = DEFAULT_PAGE_SIZE  / allocating_pool->obj_size;
     for (uint64_t i = 0; i < num_objs; i++) {
       struct pool_node *pn = (struct pool_node *) ((uint8_t *)free_page + (i * allocating_pool->obj_size));
@@ -17,6 +19,7 @@ void *pool_alloc(struct pool *allocating_pool) {
     }
   }
   void *free_space = allocating_pool->free_list;
+  printk("pool_alloc: returning object at %p\n", free_space);
   allocating_pool->free_list = allocating_pool->free_list->next;
   return free_space;
 }
