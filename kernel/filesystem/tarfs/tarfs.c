feed0e609230179ea122bbfb839e0d6a8ac8a253
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-04-14 22:36:57 -0400

    add logs

--- kernel/filesystem/tarfs/tarfs.c
+++ kernel/filesystem/tarfs/tarfs.c
@@ -6,5 +6,6 @@
 struct superblock_t *tarfs_mount(void *data, uint64_t size) {
   struct superblock_t *tarfs_superblock = superblock_t_alloc();
   parse_tar(data, size, tarfs_superblock);
+  printk("tarfs_mount: %lld\n", tarfs_superblock->root_vnode->permission_mode);
   return tarfs_superblock;
 }
