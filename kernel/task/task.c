42f643f8e999c45cc75c405d3cbd25e6d3be2f96
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-04-17 16:53:56 -0400

    create task

--- kernel/task/task.c
+++ kernel/task/task.c
@@ -1,6 +1,6 @@
 #include "kernel/task/task.h"
 
 struct task_struct *create_init_process() {
-  struct task_struct *init_task = task_alloc();
+  struct task_struct_t *init_task = task_t_alloc();
   return init_task;
 }
