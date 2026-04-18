ea0658587c74b97ea52ca09b699b800942ea7018
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-04-18 00:11:01 -0400

    fix

--- kernel/main.c
+++ kernel/main.c
@@ -61,6 +61,6 @@ void kmain(void *dtb_ptr) {
   char buf[64];
   int64_t bytes_read = target->vnode->ops->read(target->vnode, buf, 0, 63);
   buf[bytes_read] = '\0';
-  printk("printing contents of /bin/rc\n%s", (char *)PHYS_TO_VIRT(vfs_get_page(target->vnode, 0));
+  printk("printing contents of /bin/rc\n%s", (char *)PHYS_TO_VIRT(vfs_get_page(target->vnode, 0)));
   arch_wait();
 }
