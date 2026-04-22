6e0a443bb907e286094196c57ef1229fa93a5e0e
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-04-22 13:25:36 -0400

    move mount

--- kernel/filesystem/tarfs/tarfs.c
+++ kernel/filesystem/tarfs/tarfs.c
@@ -5,14 +5,6 @@
 #include "lib/printk/printk.h"
 #include "lib/string.h"
 
-struct superblock_t *tarfs_mount(void *data, uint64_t size) {
-  struct superblock_t *tarfs_superblock = superblock_t_alloc();
-  tarfs_superblock->vnode_ops.read = tarfs_vnode_read;
-  tarfs_superblock->vnode_ops.lookup = tarfs_vnode_lookup;
-  parse_tar(data, size, tarfs_superblock);
-  return tarfs_superblock;
-}
-
 int64_t tarfs_vnode_read(struct vnode_t *vnode, void *buffer, uint64_t offset, uint64_t size) {
   struct tarfs_vnode_t *tarfs_vnode = vnode->fs_private_vnode;
 
@@ -29,9 +21,17 @@ int64_t tarfs_vnode_read(struct vnode_t *vnode, void *buffer, uint64_t offset, u
   return (int64_t)size;
 }
 
-
 int64_t tarfs_vnode_lookup(const char *name, struct vnode_t *parent_dir, struct dentry_t **out) {
   // All tarfs shouldve been loaded into memory at mount time, thus if vfs reaches out to this func, the entry does not exist
   *out = NULL;
   return -1;
 }
+
+struct superblock_t *tarfs_mount(void *data, uint64_t size) {
+  struct superblock_t *tarfs_superblock = superblock_t_alloc();
+  tarfs_superblock->vnode_ops.read = tarfs_vnode_read;
+  tarfs_superblock->vnode_ops.lookup = tarfs_vnode_lookup;
+  parse_tar(data, size, tarfs_superblock);
+  return tarfs_superblock;
+}
+
