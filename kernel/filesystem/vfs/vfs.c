7ce324d9888b9d57633e0a53c84dc64c80a339aa
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-04-22 10:38:45 -0400

    fix

--- kernel/filesystem/vfs/vfs.c
+++ kernel/filesystem/vfs/vfs.c
@@ -18,7 +18,7 @@ int32_t vfs_resolve_path(const char *path, struct dentry_t **out) {
   int name_len = str_tok_no_delim(&current_path, current_name, '/', 256);
   name_len = str_tok_no_delim(&current_path, current_name, '/', 256);
   while (name_len > 0) {
-    ret = vfs_lookup(current_name, curr_dentry, &next_dentry);
+    ret = vfs_lookup(current_name, curr_dentry->vnode, &next_dentry);
     if (ret != 0 || next_dentry == NULL) {
       return ret;
     }
@@ -29,33 +29,6 @@ int32_t vfs_resolve_path(const char *path, struct dentry_t **out) {
   return 0;
 }
 
-int32_t vfs_lookup(const char *name, struct dentry_t *parent_dir, struct dentry_t **out) {
-  if (parent_dir == NULL) {
-    panic("vfs_lookup: parent_dir is NULL\n");
-  }
-
-  if (!IS_DIR(parent_dir->vnode->permission_mode)) {
-    panic("vfs_lookup: parent_dir is not a directory\n");
-  }
-
-  /*
-  * Note: This is pre- vfs abstraction, at this moment tarfs is only being used
-  * Thus everything will be in memory, so tarfs has no fs specific handlers. This
-  * needs to be abstracted to allow for other fs to hook into it, ie inode->i_ops->lookup()
-  * First search for the dentry in the in memory dcache (should have negative dentries, and positive dentries)
-  * if not found as neg dentry or pos dentry must ask underlying fs via method above
-  */
-
-  list_for_each(&parent_dir->vnode->children_dentries, pos) {
-    struct dentry_t *dentry = container_of(pos, struct dentry_t, sibling_dentry);
-    if (strncmp(dentry->name, name) == 0) {
-      *out = dentry;
-      return 0;
-    }
-  }
-  *out = NULL;
-  return -1;
-}
 
 void *vfs_get_page(struct vnode_t *vnode, size_t offset){
   // Align offset to page boundary
