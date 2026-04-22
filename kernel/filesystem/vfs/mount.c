aa0d328f82b573cffe2a51dc90239c7700dc45a5
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-04-22 18:34:58 -0400

    fix

--- kernel/filesystem/vfs/mount.c
+++ kernel/filesystem/vfs/mount.c
@@ -41,13 +41,13 @@ void vfs_init() {
   mount_list.next = &mount_list;
   mount_list.prev = &mount_list;
 
-  struct mount_t *tarfs_mount_obj = mount_t_alloc();
-  tarfs_mount_obj->root_path[0] = '/';
-  tarfs_mount_obj->root_path[1] = '\0';
-  tarfs_mount_obj->superblock = tarfs_mount((void *) _tarfs_start, (uint64_t) _tarfs_size);
+  base_mount = mount_t_alloc();
+  base_mount->root_path[0] = '/';
+  base_mount->root_path[1] = '\0';
+  base_mount->superblock = tarfs_mount((void *) _tarfs_start, (uint64_t) _tarfs_size);
 
   // Add to mount list
-  list_append(&mount_list, &tarfs_mount_obj->sibling_mount);
+  list_append(&mount_list, &base_mount->sibling_mount);
 
   struct mount_t *devfs_mount_obj = mount_t_alloc();
   strncpy(devfs_mount_obj->root_path, "/dev", 256);
