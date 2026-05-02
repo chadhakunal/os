4974a1fa4f3484446e5944e1dbe908df979c1b5d
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-05-02 14:00:28 -0400

    add in a debug print

--- kernel/include/lib/printk/printk.h
+++ kernel/include/lib/printk/printk.h
@@ -6,4 +6,15 @@
 
 void printk(const char *fmt, ...);
 
+// Include Debug in macro before including printk and set to 1 like so "#define DEBUG 1\n#include "lib/printk/printk.h"
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+
+#if DEBUG
+#define debugk(fmt, ...) printk("[DEBUG] %s:%d: " fmt, __FILE__, __LINE__, ##__VA_ARGS__)
+#else
+#define debugk(fmt, ...) ((void)0)
+#endif
+
 #endif
