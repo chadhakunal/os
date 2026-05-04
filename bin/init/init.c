89ac6eaa10ebf7121f093e490af2e0fc50477e83
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-05-03 23:23:07 -0400

    fix

--- bin/init/init.c
+++ bin/init/init.c
@@ -7,7 +7,7 @@ int main() {
   if (pid == 0) {
     printf("Child! yielding then I'm killing myself!\n");
     sched_yield();
-    printf("Child! Back from yielding\n")
+    printf("Child! Back from yielding\n");
     return 0;
   } else {
     printf("Parent! I'm waiting for %llu\n", pid);
