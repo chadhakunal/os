8dabf505545e0df21a4c53dbc282f25e791ce305
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-05-03 22:57:02 -0400

    fix

--- bin/init/init.c
+++ bin/init/init.c
@@ -6,7 +6,7 @@ int main() {
   pid_t pid = fork();
   if (pid == 0) {
     printf("Child! I'm killing myself!\n");
-    return;
+    return 0;
   } else {
     printf("Parent! I'm waiting for %llu\n", pid);
     int wstatus;
