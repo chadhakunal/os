da7c82b35cc0a60aa8b953e6ffc779232f680fc0
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-05-03 23:21:49 -0400

    implement yield

--- kernel/syscalls/yield.c
+++ kernel/syscalls/yield.c
@@ -1,8 +1,13 @@
 #include "arch/riscv64/syscalls/syscall_macros.h"
 #include "arch/riscv64/syscalls/syscalls.h"
 #include "types.h"
+#include "kernel/task/schedule.h"
+#include "kernel/task/task.h"
 
 DEFINE_SYSCALL0(sched_yield)
 {
+  // Force expiration by setting runtime to max
+  current_task->runtime = current_task->max_runtime;
+  schedule();
   return 0;
 }
