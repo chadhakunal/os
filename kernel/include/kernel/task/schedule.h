f81c8a81f65de648e50ffeec59959cb1f3c4c64a
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-05-03 20:06:11 -0400

    fix

--- kernel/include/kernel/task/schedule.h
+++ kernel/include/kernel/task/schedule.h
@@ -17,6 +17,8 @@ extern struct scheduler_t scheduler;
 
 void schedule();
 
+void init_scheduler();
+
 // Function that new tasks jump to after their first switch_to
 // This is set up as a fake return address on new task's kernel stack
 void fresh_task_jump(void);
