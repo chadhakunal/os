72cea71c2cc11668995703204d739bf712884ded
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-05-04 02:46:31 -0400

    fix

--- kernel/arch/riscv64/trap.c
+++ kernel/arch/riscv64/trap.c
@@ -58,6 +58,10 @@ void trap_handler(struct trap_frame *tf) {
         if (tf->sstatus & (1UL << 8)) {
           printk("[trap] Came from kernel mode, about to return\n");
           printk("[trap] sepc=%llx, returning now...\n", tf->sepc);
+          printk("[trap] About to return from C function...\n");
+          // Force a nop to ensure we're actually executing
+          asm volatile("nop");
+          printk("[trap] Executed nop, now returning...\n");
           return;
         }
 
