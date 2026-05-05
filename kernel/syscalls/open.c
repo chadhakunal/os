fda5258c4f6d79cb36534f55a14f48720b1eaf43
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-05-05 01:58:11 -0400

    remove supporot for dirfd

--- kernel/syscalls/open.c
+++ kernel/syscalls/open.c
@@ -14,39 +14,14 @@ DEFINE_SYSCALL4(openat, int, dirfd, const char *, user_path, uint64_t, flags, ui
   char path[MAX_PATH_COPY];
   copy_string_from_user(path, user_path, MAX_PATH_COPY);
 
-  char resolved_path[MAX_PATH_COPY];
-
-  // If path is absolute, ignore dirfd
-  if (path[0] == '/') {
-    strncpy(resolved_path, path, MAX_PATH_COPY);
-  }
-  // If dirfd is AT_FDCWD, use path as-is (relative to CWD)
-  else if (dirfd == AT_FDCWD) {
-    strncpy(resolved_path, path, MAX_PATH_COPY);
-  }
-  // Otherwise, resolve relative to dirfd
-  else {
-    struct file_t *dir_file = find_file(&current_task->file_table, dirfd);
-    if (!dir_file || !dir_file->dentry) {
-      return -1;
-    }
-
-    // Build path: dirfd's path + "/" + relative path
-    vfs_get_absolute_path(dir_file->dentry, resolved_path, MAX_PATH_COPY);
-    int dir_len = str_len(resolved_path, MAX_PATH_COPY);
-
-    // Add "/" if not already there
-    if (dir_len > 0 && resolved_path[dir_len - 1] != '/') {
-      resolved_path[dir_len] = '/';
-      dir_len++;
-    }
-
-    // Append relative path
-    strncpy(resolved_path + dir_len, path, MAX_PATH_COPY - dir_len);
+  // For now, only support absolute paths and AT_FDCWD
+  // TODO: Implement relative path resolution with dirfd
+  if (path[0] != '/' && dirfd != AT_FDCWD) {
+    return -1;  // Not supported yet
   }
 
   struct file_t *file;
-  int ret = vfs_open(resolved_path, flags, &file);
+  int ret = vfs_open(path, flags, &file);
   if (ret != 0) {
     return -1;
   }
