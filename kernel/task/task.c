2066bf7910a8e34f957c6599d73d947e94bd0db9
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-05-04 21:32:26 -0400

    log

--- kernel/task/task.c
+++ kernel/task/task.c
@@ -513,6 +513,7 @@ void clear_vmas(struct task_t *task) {
 }
 
 void close_all_files(struct task_t *task) {
+  printk("Closing files\n");
   struct list_node *pos = task->file_table.files_list.next;
   while (pos != &task->file_table.files_list) {
     struct files_list_t *files_list = container_of(pos, struct files_list_t, files_list);
