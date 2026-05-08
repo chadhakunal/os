7be51fabd1375dc82dc996761d0eb850447a2acb
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-05-07 21:51:22 -0400

    add test

---
+++ bin/getcwd_test/getcwd_test.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+  char buf[256];
+
+  printf("Testing getcwd...\n");
+
+  char *result = getcwd(buf, sizeof(buf));
+  if (result == NULL) {
+    printf("getcwd failed\n");
+    return 1;
+  }
+
+  printf("Current working directory: %s\n", buf);
+
+  if (buf[0] != '/') {
+    printf("Error: path doesn't start with /\n");
+    return 1;
+  }
+
+  printf("getcwd test passed\n");
+  return 0;
+}
