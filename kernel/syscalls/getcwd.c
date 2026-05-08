ab111911a5a6795a028bfa215995bb7f6e4c745c
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-05-07 23:05:10 -0400

    debug

--- kernel/syscalls/getcwd.c
+++ kernel/syscalls/getcwd.c
@@ -4,13 +4,14 @@
 #include "kernel/filesystem/vfs/vfs.h"
 #include "errno.h"
 #include "types.h"
+#include "lib/printk/printk.h"
 
 DEFINE_SYSCALL2(getcwd, char *, buf, size_t, size)
 {
   if (buf == NULL || size == 0) {
     return -EINVAL;
   }
-
+  debugk("cwd: %s\n", current_task->cwd);
   int64_t res = vfs_dentry_get_path(current_task->cwd, buf, size);
 
   if (res < 0) {
