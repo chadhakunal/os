a47b1d863a84b9a3abba136be717b50d53e733aa
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-05-07 22:36:08 -0400

    add name

--- kernel/syscalls/chdir.c
+++ kernel/syscalls/chdir.c
@@ -38,7 +38,7 @@ DEFINE_SYSCALL1(chdir, const char *, user_path)
   }
 
   current_task->cwd = dentry;
-  debugk("chdir: success, new cwd=%p\n", current_task->cwd);
+  debugk("chdir: success, new cwd=%p name=%s\n", current_task->cwd->name);
 
   return 0;
 }
