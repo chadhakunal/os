9f9c51ae97bcb805579f8c79cf12daa4be9f8347
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-05-11 22:57:51 -0400

    fic

--- bin/stacktest/stacktest.c
+++ bin/stacktest/stacktest.c
@@ -17,7 +17,7 @@ void fill_stack(int n) {
     fill_stack(n - 1);
   }
   buffer[0] = 'h';
-  printf("%s\n", buffer[0]);
+  printf("%s\n", &buffer[0]);
   printf("Returning from depth %d\n", depth);
   depth--;
 }
