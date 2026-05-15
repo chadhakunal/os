d420b67bf3ded27b23243afe3ad7d6220543d63e
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-05-14 21:12:59 -0400

    alloc a lot more !

--- bin/stacktest/stacktest.c
+++ bin/stacktest/stacktest.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-  char buf[12288]; /* 3 pages: first 2 eager, 3rd triggers lazy page fault */
+  char buf[49152]; /* 3 pages: first 2 eager, 3rd triggers lazy page fault */
 
-  for (int i = 0; i < 12288; i++)
+  for (int i = 0; i < 49152; i++)
     buf[i] = (char)i;
-  for (int i = 0; i < 12288; i++) {
+  for (int i = 0; i < 49152; i++) {
     if (i % 254 == 0) printf("buf[%d] = %d\n", i,buf[i]);
   }
   printf("Stack test passed: buf[0]=%d\n", buf[0]);
