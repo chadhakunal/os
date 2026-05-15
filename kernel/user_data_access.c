72987fea49f6a2c0f1518d184caceaa88536d496
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-05-14 22:15:42 -0400

    fix

--- kernel/user_data_access.c
+++ kernel/user_data_access.c
@@ -6,6 +6,9 @@
 #include "lib/string.h"
 #include "lib/printk/printk.h"
 
+extern int kernel_fault_recovery_setup(struct kernel_fault_recovery_t *r);
+extern void kernel_fault_recovery_jump(struct kernel_fault_recovery_t *r);
+
 static bool is_user_address(uint64_t addr, size_t len) {
   if (addr >= END_USER_SPACE_ADDR) {
     return false;
@@ -25,8 +28,14 @@ int copy_to_user(void *user_dest, const void *kernel_src, size_t n) {
   asm volatile("csrr %0, sstatus" : "=r"(old_sstatus));
   asm volatile("csrs sstatus, %0" :: "r"(SSTATUS_SUM));
 
+  if (kernel_fault_recovery_setup(&current_task->fault_recovery)) {
+    asm volatile("csrw sstatus, %0" :: "r"(old_sstatus));
+    return -1;
+  }
+
   memcpy(user_dest, kernel_src, n);
 
+  current_task->fault_recovery.active = 0;
   asm volatile("csrw sstatus, %0" :: "r"(old_sstatus));
   return 0;
 }
@@ -40,8 +49,14 @@ int copy_from_user(void *kernel_dest, const void *user_src, size_t n) {
   asm volatile("csrr %0, sstatus" : "=r"(old_sstatus));
   asm volatile("csrs sstatus, %0" :: "r"(SSTATUS_SUM));
 
+  if (kernel_fault_recovery_setup(&current_task->fault_recovery)) {
+    asm volatile("csrw sstatus, %0" :: "r"(old_sstatus));
+    return -1;
+  }
+
   memcpy(kernel_dest, user_src, n);
 
+  current_task->fault_recovery.active = 0;
   asm volatile("csrw sstatus, %0" :: "r"(old_sstatus));
   return 0;
 }
