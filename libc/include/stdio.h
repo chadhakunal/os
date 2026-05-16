60fb64bfb4b89797e3352b88e108c3c103c6d0ae
chadhakunal <chadhakunal1224@gmail.com>
2026-05-16 12:02:40 -0400

    moving tests to thirdparty

--- libc/include/stdio.h
+++ libc/include/stdio.h
@@ -2,8 +2,27 @@
 #define STDIO_H
 
 #include <stddef.h>
+#include <stdarg.h>
 
-int printf(const char *, ...);
+#define EOF (-1)
+
+typedef struct FILE {
+  int fd;
+} FILE;
+
+extern FILE __stdin_file;
+extern FILE __stdout_file;
+extern FILE __stderr_file;
+
+#define stdin  (&__stdin_file)
+#define stdout (&__stdout_file)
+#define stderr (&__stderr_file)
+
+int printf(const char *fmt, ...);
+int fprintf(FILE *stream, const char *fmt, ...);
+int vfprintf(FILE *stream, const char *fmt, va_list ap);
+int fputs(const char *s, FILE *stream);
+int fputc(int c, FILE *stream);
 int snprintf(char *str, size_t size, const char *format, ...);
 
 #endif
