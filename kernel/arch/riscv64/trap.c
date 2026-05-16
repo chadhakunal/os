d2daa34d3576e930b97485101b263f7d48383544
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-05-16 18:27:14 -0400

    revert back

--- kernel/arch/riscv64/trap.c
+++ kernel/arch/riscv64/trap.c
@@ -86,7 +86,6 @@ void trap_handler(struct trap_frame *tf) {
       case 12:
       case 13:
       case 15:
-        panic("Page fault!\n");
         handle_page_fault(tf->stval, cause_code, tf);
         if (tf->sstatus & SSTATUS_SPP) {
           return; // kernel-mode fault (e.g. copy_to/from_user): trap_vector restores kernel context from stack
