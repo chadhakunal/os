fa36748f74e489d1be3d1c3b39fcb7c74187592b
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-05-16 20:23:33 -0400

    update print_failed

--- print_failed.sh
+++ print_failed.sh
@@ -1,5 +1,5 @@
-tests="fclose fdopen feof ferror fflush fgetc fgetpos fgets fileno flockfile fmemopen fopen fprintf"
+tests="psiginfo psignal pthread_kill pthread_sigmask raise sig2str sigaction sigaltstack sigpending sigprocmask sigqueue sigsuspend sigtimedwait sigwait sigwaitinfo str2sig"
 for t in $tests; do
   echo "=== $t ==="
-  cat thirdparty/os-tests-sortix/ostest/report/stdio/$t.err 2>/dev/null || echo "(no file)"
-done >/tmp/stdio_errors.txt && echo "done"
+  cat thirdparty/os-tests-sortix/ostest/report/signal/$t.err 2>/dev/null || echo "(no file)"
+done >/tmp/signal_errors.txt && echo "done"
