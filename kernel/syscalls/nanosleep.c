b0dd9831f256ade1efb4e358ca3f2cb74d09ffd7
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2026-05-18 17:08:54 -0400

    fix nanosleep

--- kernel/syscalls/nanosleep.c
+++ kernel/syscalls/nanosleep.c
@@ -2,7 +2,9 @@
 #include "arch/riscv64/syscalls/syscalls.h"
 #include "kernel/task/task.h"
 #include "kernel/task/schedule.h"
+#include "kernel/task/signal.h"
 #include "kernel/time/timer.h"
+#include "errno.h"
 
 struct timespec {
   int64_t tv_sec;
@@ -30,6 +32,16 @@ DEFINE_SYSCALL2(nanosleep, const struct timespec *, req, struct timespec *, rem)
   current_task->state       = TASK_BLOCKED;
 
   schedule();
+
+  asm volatile("csrs sstatus, %0" :: "r"(SSTATUS_SUM));
+
+  /* If a signal woke us before the deadline, return -EINTR. */
+  sigset_t pending_unblocked = current_task->signal_state.pending
+                               & ~current_task->signal_state.blocked;
+  if (pending_unblocked) {
+    if (rem) { rem->tv_sec = 0; rem->tv_nsec = 0; }
+    return -EINTR;
+  }
 
   if (rem) { rem->tv_sec = 0; rem->tv_nsec = 0; }
   return 0;
